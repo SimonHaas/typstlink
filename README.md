e94911e18c4cb0e42cf9a1f81a5588d92c3c3c5f
Simon Haas <22013217+SimonHaas@users.noreply.github.com>
2026-04-20 22:59:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# typstlink
+
+A simple service to render [typst](https://typst.app) documents to pdf inside a browser via a link like: https://typst.link?url=https://typst.app
---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const port = process.env.PORT || 8080;
+
+http
+  .createServer((req, res) => {
+    const url = req.url;
+    console.log(`Received request for ${url}`);
+
+    try {
+      if (url.match(/\/typst.*/)) {
+        res.writeHead(200, {
+          "Content-Type": "text/plain",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        });
+
+        const params = new URL(
+          `http://${process.env.HOST ?? "localhost"}${req.url}`,
+        ).searchParams;
+        const url = params.get("url");
+        const response = fetch(url).then((response) =>
+          response.text().then((typstContent) => {
+            res.write(typstContent);
+            res.end();
+          }),
+        );
+      }
+    } catch (error) {
+      res.writeHead(500, {
+        "Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      });
+      res.write("Error fetching the document.");
+      res.end();
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+  });
